865d81f924a5ee533a04f1ef9ec4dc21b177635e
Huy Vo <huyvanvo94@gmail.com>
2020-11-16 18:55:02 -0800

    update

--- test/test_jwt_helper.rb
+++ test/test_jwt_helper.rb
@@ -5,4 +5,16 @@ class TestJwtHelper < ActiveSupport::TestCase
   test 'it works' do
     assert 1 == 1
   end
+
+  test 'it works 2' do
+    token = JwtHelper.encode({ :test => 'test' }, Time.now.to_i + 30.minutes)
+    assert token != nil
+    assert JwtHelper.decode(token)['test'] == 'test'
+  end
+
+  test 'it doe not work' do
+    token = JwtHelper.encode({ :test => 'test' }, -1000)
+    assert token != nil
+    assert JwtHelper.decode(token) == nil
+  end
 end
